14a61d1d4bd4cb714840609dd23b219f0242b866
aaravbhatt <aarav.bhatt.uk@gmail.com>
2025-12-16 23:10:46 +0000

    added snap validation

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,22 +1,34 @@
 extends Node2D
 
 @onready var tile_map = $TileMapLayer
+
+var onBoard = {}
 	
 # Converts pixel position to grid
 func get_grid_coords(pixel_pos: Vector2):
 	var local_pos = tile_map.to_local(pixel_pos)
-	print ("local", local_pos)
 	var map_pos = tile_map.local_to_map(local_pos)
-	#map_pos.x += 6
-	#map_pos.y *= -1
-	#map_pos.y += 7
+	map_pos.y *= -1
+	map_pos.y += 8
 	return map_pos
 
 # Converts grid to pixel center
 func get_slot_center(grid_pos: Vector2i):
-	#grid_pos.x -= 6
-	#grid_pos.y -= 7
-	#grid_pos.y *= -1
+	grid_pos.y -= 8
+	grid_pos.y *= -1
 	var local_center = tile_map.map_to_local(grid_pos)
-	print ("local2", local_center)
 	return tile_map.to_global(local_center)
+	
+func place(gridPos, tile_ref): # Pass 'self' from the tile
+	onBoard[gridPos] = tile_ref
+		
+func isValidGridPos(gridPos, requesting_tile):
+	# Check if someone ELSE is in this spot
+	if onBoard.has(gridPos) and onBoard[gridPos] != requesting_tile: 
+		return false
+	# Bounds check
+	return 0 <= gridPos.x and gridPos.x <= 8 and 0 <= gridPos.y and gridPos.y <= 8
+		
+func remove(gridPos):
+	onBoard.erase(gridPos)
+		
--- scripts/tile.gd
+++ scripts/tile.gd
@@ -6,6 +6,7 @@ extends Area2D
 @onready var label = $Label
 
 var dragging = false
+var prevPos
 
 @export var letter_text: String = "A":
 	set(value):
@@ -19,24 +20,31 @@ var dragging = false
 func _input_event(_viewport, event, _shape_idx):
 	# Detect Mouse Down
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		
 		if event.pressed:
+			prevPos = global_position
 			dragging = true
 			z_index += 20
 			visual.position -= Vector2(10, 10)
-			
+			# Makes sure that no more tiles below get dragged
 			shadow.visible = true
 		else:
+			if (dragging == false): return
 			dragging = false
 			shadow.visible = false
 			z_index -= 20
 			visual.position += Vector2(10, 10)
-			snap_to_grid()
+			if (not snap_to_grid()):
+				position = prevPos
+		get_viewport().set_input_as_handled()
 
 func _ready():
+	prevPos = global_position 
 	rescale_text()
 
 func _process(_delta):
 	if dragging:
+		
 		global_position = get_global_mouse_position()
 
 func snap_to_grid():
@@ -47,9 +55,20 @@ func snap_to_grid():
 		
 		var grid_pos = board.get_grid_coords(mouse_pos)
 		
+		if (not board.isValidGridPos(grid_pos, self)):
+			return false
+		
 		var global_center = board.get_slot_center(grid_pos)
 		
 		global_position = global_center
+		
+		var prev_grid_pos = board.get_grid_coords(prevPos)
+		board.remove(prev_grid_pos)
+		prevPos = global_position
+		board.place(grid_pos, self)
+		
+		return true
+	return false
 		
 func rescale_text():
 	var label = get_node_or_null("BlockBg/Label")
